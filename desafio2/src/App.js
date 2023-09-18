9c4d708ca5fca3a53f9f9fc445132f3503bc6568
lysgut <88504963+elixxi@users.noreply.github.com>
2023-09-18 16:32:17 -0300

    avanceee

--- desafio2/src/App.js
+++ desafio2/src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  // Estado para almacenar la fecha actual
+  const [fecha, setFecha] = useState(new Date());
+
+  // Estado para almacenar la cantidad de avances (steps)
+  const [cantidadAvances, setCantidadAvances] = useState(1);
+
+  // Función para retroceder un día
+  const retrocederDia = () => {
+    const nuevaFecha = new Date(fecha);
+    nuevaFecha.setDate(fecha.getDate() - 1);
+    setFecha(nuevaFecha);
+  };
+
+  // Función para avanzar un día
+  const avanzarDia = () => {
+    const nuevaFecha = new Date(fecha);
+    nuevaFecha.setDate(fecha.getDate() + 1);
+    setFecha(nuevaFecha);
+  };
+
+  // Función para incrementar la cantidad de avances (steps)
+  const incrementarCantidadAvances = () => {
+    setCantidadAvances(cantidadAvances + 1);
+  };
+
+  // Función para decrementar la cantidad de avances (steps)
+  const decrementarCantidadAvances = () => {
+    if (cantidadAvances > 1) {
+      setCantidadAvances(cantidadAvances - 1);
+    }
+  };
+
+  // Función para avanzar la cantidad de días especificados
+  const avanzarDiasConCantidad = (cantidad) => {
+    const nuevaFecha = new Date(fecha);
+    nuevaFecha.setDate(fecha.getDate() + cantidad);
+    setFecha(nuevaFecha);
+  };
+
+  // Función para retroceder la cantidad de días especificados
+  const retrocederDiasConCantidad = (cantidad) => {
+    const nuevaFecha = new Date(fecha);
+    nuevaFecha.setDate(fecha.getDate() - cantidad);
+    setFecha(nuevaFecha);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bienvenidos</h1>
+      <p>Fecha Actual: {fecha.toLocaleDateString()}</p>
+      <div>
+        <button onClick={decrementarCantidadAvances}>-</button>
+        <span>{cantidadAvances}</span>
+        <button onClick={incrementarCantidadAvances}>+</button>
+      </div>
+      <button onClick={() => avanzarDiasConCantidad(cantidadAvances)}>Avanzar con Cantidad</button>
+      <button onClick={() => retrocederDiasConCantidad(cantidadAvances)}>Retroceder con Cantidad</button>
     </div>
   );
 }
