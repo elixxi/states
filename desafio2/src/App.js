82ba97dfe6051ee90df7595c1965e5ba93cdd206
lysgut <88504963+elixxi@users.noreply.github.com>
2023-09-18 21:34:31 -0300

    casiiiiii

--- desafio2/src/App.js
+++ desafio2/src/App.js
@@ -1,13 +1,79 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
+const flechaUrl = 'https://cdn-icons-png.flaticon.com/512/860/860774.png';
+
+// URL del segundo logo
+const segundoLogoUrl = 'https://cdn-icons-png.flaticon.com/512/54/54531.png';
+
+function ButtonsArriba({ cantidadAvances, incrementarCantidadAvances, decrementarCantidadAvances }) {
+  return (
+    <div className="mb-3">
+      <img
+        src="https://cdn-icons-png.flaticon.com/512/860/860774.png"
+        alt="Botón de Disminución"
+        onClick={decrementarCantidadAvances}
+        style={{ width: "20px", height: "20px", cursor: "pointer" }}
+      />   
+        <span className="mx-3">{cantidadAvances}</span>
+        <img
+        src="https://cdn-icons-png.flaticon.com/512/54/54531.png"
+        alt="Botón de Aumento"
+        onClick={incrementarCantidadAvances}
+        style={{ width: "20px", height: "20px", cursor: "pointer" }}
+      />
+
+    </div>
+  );
+}
+
+// Componente para los botones de abajo
+function ButtonsAbajo({ cantidadAvances, avanzarDiasConCantidad, retrocederDiasConCantidad }) {
+  return (
+    <div className="d-flex justify-content-center align-items-center">
+      <img
+        src={flechaUrl}
+        alt='Flecha izquierda'
+        onClick={() => retrocederDiasConCantidad(cantidadAvances)}
+        style={{ width: '20px', height: '20px', cursor: 'pointer' }}
+      />
+      <span className="mx-3">{cantidadAvances}</span>
+      <img
+        src={segundoLogoUrl}
+        alt='Flecha derecha'
+        onClick={() => avanzarDiasConCantidad(cantidadAvances)}
+        style={{ width: '20px', height: '20px', cursor: 'pointer' }}
+      />
+    </div>
+  );
+}
 
 function App() {
+  // Estado para almacenar la hora actual
+  const [horaActual, setHoraActual] = useState(new Date().getHours());
+
+  // Estado para almacenar la URL de la imagen del sol o la luna
+  const [imagenUrl, setImagenUrl] = useState('');
+
   // Estado para almacenar la fecha actual
   const [fecha, setFecha] = useState(new Date());
 
   // Estado para almacenar la cantidad de avances (steps)
   const [cantidadAvances, setCantidadAvances] = useState(1);
+
+  useEffect(() => {
+    // Función para determinar si es de día o de noche
+    const esDeDia = (hora) => {
+      return hora >= 6 && hora < 18; // Suponemos que el día es de 6 AM a 6 PM
+    };
+
+    // Determine si es de día o de noche y configure la URL de la imagen en consecuencia
+    if (esDeDia(horaActual)) {
+      setImagenUrl('https://i0.pngocean.com/files/985/682/370/cartoon-smiling-sun.jpg'); // Cambia 'url_de_la_imagen_del_sol.jpg' por la URL de la imagen del sol
+    } else {
+      setImagenUrl('https://i.pinimg.com/originals/32/3e/9d/323e9d46bf8ea3a9f48637657feb104a.png'); // Cambia 'url_de_la_imagen_de_la_luna.jpg' por la URL de la imagen de la luna
+    }
+  }, [horaActual]);
 
   // Función para retroceder un día
   const retrocederDia = () => {
@@ -50,16 +116,28 @@ function App() {
   };
 
   return (
-    <div className="App container mt-5">
-      <h1>Bienvenidos</h1>
-      <div className="mb-3">
-        <button className="btn btn-danger" onClick={decrementarCantidadAvances}>-</button>
-        <span className="mx-3">{cantidadAvances}</span>
-        <button className="btn btn-danger" onClick={incrementarCantidadAvances}>+</button>
+    <div className="App mt-5">
+      <div className="content-container">
+      <h1 className="titulo-bienvenida">Bienvenidos</h1>
+        <ButtonsArriba
+          cantidadAvances={cantidadAvances}
+          incrementarCantidadAvances={incrementarCantidadAvances}
+          decrementarCantidadAvances={decrementarCantidadAvances}
+        />
+        <ButtonsAbajo
+          cantidadAvances={cantidadAvances}
+          avanzarDiasConCantidad={avanzarDiasConCantidad}
+          retrocederDiasConCantidad={retrocederDiasConCantidad}
+        />
+ <p className="fecha-actual">{fecha.toLocaleDateString()}</p>
+        <div className="image-container my-4">
+          <img
+            src={imagenUrl}
+            alt="Imagen de día o noche"
+            className="center-image small-image"
+          />
+        </div>
       </div>
-      <button className="btn btn-danger" onClick={() => avanzarDiasConCantidad(cantidadAvances)}>+</button>
-      <button className="btn btn-danger" onClick={() => retrocederDiasConCantidad(cantidadAvances)}>-</button>
-      <p>Fecha Actual: {fecha.toLocaleDateString()}</p>
     </div>
   );
 }
